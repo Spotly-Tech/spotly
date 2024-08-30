3f17451566ec97c21cf45047f24b6774a88d946f
mBukator <m.bukator03@gmail.com>
2024-08-30 20:38:10 +0200

    [Feat]: Create custom cursor

--- src/components/CustomCursor/index.tsx
+++ src/components/CustomCursor/index.tsx
@@ -1,18 +1,81 @@
-import { useEffect, useRef } from 'react'
+import { FC, MutableRefObject, useEffect, useState } from 'react'
+
+import { motion, useMotionValue, useSpring } from 'framer-motion'
+
 import styles from './CustomCursor.module.css'
 
-const CustomCursor = () => {
-	const cursorRef = useRef(null)
+interface ICustomCursorProps {
+    stickyElement: MutableRefObject<HTMLElement | null>
+}
 
-	useEffect(() => {
-		document.addEventListener('mousemove', (e) => {
-			const { clientX, clientY } = e
-			const mouseX = clientX - cursorRef.current.clientWidth / 2
-			const mouseY = clientY - cursorRef.current.clientHeight / 2
-			cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`
-		})
-	})
-	return <div className={styles.app_cursor} ref={cursorRef} />
+const CustomCursor: FC<ICustomCursorProps> = ({ stickyElement }) => {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const cursorSize = isHovered ? 90 : 40
+
+    const mouse = {
+        x: useMotionValue(0),
+        y: useMotionValue(0),
+    }
+
+    const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 }
+    const smoothMouse = {
+        x: useSpring(mouse.x, smoothOptions),
+        y: useSpring(mouse.y, smoothOptions),
+    }
+
+    const manageMouseMove = (e: MouseEvent) => {
+        const { clientX, clientY } = e
+        if (stickyElement.current) {
+            const { left, top, width, height } =
+                stickyElement.current.getBoundingClientRect()
+
+            const center = { x: left + width / 2, y: top + height / 2 }
+            const distance = { x: clientX - center.x, y: clientY - center.y }
+
+            if (isHovered) {
+                mouse.x.set(center.x - cursorSize / 2 + distance.x * 0.1)
+                mouse.y.set(center.y - cursorSize / 2 + distance.y * 0.1)
+            } else {
+                mouse.x.set(clientX - cursorSize / 2)
+                mouse.y.set(clientY - cursorSize / 2)
+            }
+        }
+    }
+
+    const manageMouseOver = () => {
+        setIsHovered(true)
+    }
+    const manageMouseLeave = () => {
+        setIsHovered(false)
+    }
+
+    useEffect(() => {
+        const currentElement = stickyElement.current
+
+        window.addEventListener('mousemove', manageMouseMove)
+
+        currentElement?.addEventListener('mouseover', manageMouseOver)
+        currentElement?.addEventListener('mouseleave', manageMouseLeave)
+        return () => {
+            window.removeEventListener('mousemove', manageMouseMove)
+
+            currentElement?.removeEventListener('mouseover', manageMouseOver)
+            currentElement?.removeEventListener('mouseleave', manageMouseLeave)
+        }
+    })
+
+    return (
+        <motion.div
+            className={styles.app_cursor}
+            style={{
+                left: smoothMouse.x,
+                top: smoothMouse.y,
+                backgroundColor: isHovered ? 'black' : 'transparent',
+            }}
+            animate={{ width: cursorSize, height: cursorSize }}
+        ></motion.div>
+    )
 }
 
 export default CustomCursor
