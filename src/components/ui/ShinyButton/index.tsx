88ff2a9e7ff0ccfc87b9ca06dec7447cea4cf7e7
mBukator <m.bukator03@gmail.com>
2024-08-30 21:18:18 +0200

    [Feat]: Create shiny button

---
+++ src/components/ui/ShinyButton/index.tsx
@@ -0,0 +1,41 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+
+import { motion } from 'framer-motion'
+
+import styles from './ShinyButton.module.css'
+
+interface ShinyButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode
+}
+
+function ShinyButton({ children, ...props }: ShinyButtonProps) {
+    return (
+        <motion.button
+            {...props}
+            className={`${styles.btn} ${styles.radial_gradient}`}
+            initial={{ '--x': '100%', scale: 1 }}
+            animate={{ '--x': '-100%' }}
+            whileTap={{ scale: 0.85 }}
+            transition={{
+                repeat: Infinity,
+                repeatType: 'loop',
+                repeatDelay: 1,
+                type: 'spring',
+                stiffness: 20,
+                damping: 15,
+                mass: 2,
+                scale: {
+                    type: 'spring',
+                    stiffness: 10,
+                    damping: 5,
+                    mass: 0.1,
+                },
+            }}
+        >
+            <span className={`${styles.btn_text} ${styles.linear_mask}`}>{children}</span>
+            <span className={`${styles.overlay} ${styles.linear_overlay}`} />
+        </motion.button>
+    )
+}
+
+export default ShinyButton
