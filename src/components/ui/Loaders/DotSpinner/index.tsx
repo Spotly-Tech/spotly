01fd05671deb4a1051b91faa81b4ee3f70722d8f
mBukator <m.bukator03@gmail.com>
2024-08-30 21:17:21 +0200

    [Feat]: Create dot spinner

---
+++ src/components/ui/Loaders/DotSpinner/index.tsx
@@ -0,0 +1,18 @@
+import styles from './DotSpinner.module.css'
+
+const DotSpinner = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+        </div>
+    )
+}
+
+export default DotSpinner
